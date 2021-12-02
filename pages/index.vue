abaa681adab9ca6d66e5226ada5f46bd14adfebd
Daniel Boros <boros.daniel@protonmail.com>
2021-12-02 22:14:42 +0100

    reformat with eslint and prettier

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,56 @@
 <template>
-  <div id='target' class="overflow-hidden font-sans">
-    <section class="fixed left-0 top-0 bottom-0 overflow-y-auto bg-white p-8 w-2/3">
+  <div id="target" class="overflow-hidden font-sans">
+    <section
+      class="fixed left-0 top-0 bottom-0 overflow-y-auto bg-white p-8 w-2/3"
+    >
       <h1 class="font-black mb-8 ml-2 pb-2">
-        <a href="/" class="no-underline text-black no-underline text-black transition text-geen">
-          <img src="/images/logo.svg" class="inline-block mr-2 w-10 -mb-2" />Stitches for tailblocks</a>
+        <a
+          href="/"
+          class="no-underline text-black no-underline text-black transition text-geen"
+        >
+          <img
+            src="/images/logo.svg"
+            class="inline-block mr-2 w-10 -mb-2"
+          />Stitches for tailblocks</a
+        >
       </h1>
       <div class="border-l-2 mx-2 mb-3 js-filter pl-3 border-black">
         <button
-          v-for='{ title, filter } in buttons'
-          :key='filter'
+          v-for="{ title, filter } in buttons"
+          :key="filter"
           class="text-black font-semibold hover:text-green-500 px-2 py-1 transition-normal"
-          @click='itemFilter = filter'
+          @click="itemFilter = filter"
         >
           {{ title }}
         </button>
       </div>
       <div
         v-dragula="items"
-        :style='(gHeight ? "height: "+ gHeight : "")'
+        :style="gHeight ? 'height: ' + gHeight : ''"
         class="flex flex-wrap flex-col items-start justify-start js-snippets"
-        drake='first'
+        drake="first"
       >
         <figure
-          v-for='item in items'
-          v-show='itemFilter === "js-snippet" || itemFilter === item.type'
-          :key='item.image'
+          v-for="item in items"
+          v-show="itemFilter === 'js-snippet' || itemFilter === item.type"
+          :key="item.image"
           class="js-snippet w-1/2 p-2"
         >
-          <img :src="`/images/${item.image}.png`" alt=""
-               class="border border-gray-300 cursor-move hover:shadow transition-normal hover:translate-y-1 hover:border-black" />
+          <img
+            :src="`/images/${item.image}.png`"
+            alt=""
+            class="border border-gray-300 cursor-move hover:shadow transition-normal hover:translate-y-1 hover:border-black"
+          />
         </figure>
       </div>
     </section>
-    <section class="bg-gray-300 p-8 pb-0 w-1/3 flex flex-col fixed top-0 bottom-0 right-0">
+    <section
+      class="bg-gray-300 p-8 pb-0 w-1/3 flex flex-col fixed top-0 bottom-0 right-0"
+    >
       <header class="mb-4">
         <button
-          class="shadow text-center py-3 px-4 bg-black w-full hover:text-green-500 text-white js-download transition-normal">
+          class="shadow text-center py-3 px-4 bg-black w-full hover:text-green-500 text-white js-download transition-normal"
+        >
           Generate HTML File with Functional CSS
         </button>
       </header>
@@ -45,23 +60,37 @@
         class="flex flex-col flex-1 border js-droppable border-gray-300 overflow-y-auto"
       >
         <figure
-          v-for='(item) in itemsDest'
-          :key='item.id'
+          v-for="item in itemsDest"
+          :key="item.id"
           class="js-snippet w-1/2 p-2 vue-element"
         >
-          <img :src="`/images/${item.image}.png`" alt=""
-               class="border border-gray-300 cursor-move hover:shadow transition-normal hover:translate-y-1 hover:border-black" />
+          <img
+            :src="`/images/${item.image}.png`"
+            alt=""
+            class="border border-gray-300 cursor-move hover:shadow transition-normal hover:translate-y-1 hover:border-black"
+          />
         </figure>
       </div>
       <div class="text-center text-black text-xs py-2">
         Stitches by
-        <a class="textgreene hover:ugreend text-green-500" target="_blank" href="https://amie-chen.com">Amie Chen</a>,
-        tailblocks by
-        <a class="textgreene hover:ugreend text-green-500" target="_blank"
-           href="https://mertjf.github.io/tailblocks/">Mert Cukuren</a>,
-        Forked by
-        <a class="textgreene hover:ugreend text-green-500" target="_blank" href="https://maienschein.tech">Fabio
-          Maienschein</a>
+        <a
+          class="textgreene hover:ugreend text-green-500"
+          target="_blank"
+          href="https://amie-chen.com"
+          >Amie Chen</a
+        >, tailblocks by
+        <a
+          class="textgreene hover:ugreend text-green-500"
+          target="_blank"
+          href="https://mertjf.github.io/tailblocks/"
+          >Mert Cukuren</a
+        >
+        , Forked by<a
+          class="textgreene hover:ugreend text-green-500"
+          target="_blank"
+          href="https://maienschein.tech"
+          >Fabio Maienschein</a
+        >
       </div>
       <div class="bg-white hidden fixed js-delete-btn px-4 py-2 shadow">
         Delete <i class="far fa-trash-alt pointer-events-none"></i>
@@ -142,41 +171,46 @@ export default {
         { image: 'testimonial-1', width: '1/2', type: 'st-testimonial' },
         { image: 'testimonial-2', width: '1/2', type: 'st-testimonial' },
         { image: 'testimonial-3', width: '1/2', type: 'st-testimonial' },
-      ]
+      ],
     }
   },
   computed: {
-    buttons(){
+    buttons() {
       return [
-        { title: 'All', filter: 'js-snippet'},
-        { title: 'Blog', filter: 'st-blog'},
-        { title: 'Contact', filter: 'st-contact'},
-        { title: 'Content', filter: 'st-content'},
-        { title: 'CTA', filter: 'st-cta'},
-        { title: 'E-Commerce', filter: 'st-ecommerce'},
-        { title: 'Feature', filter: 'st-feature'},
-        { title: 'Footer', filter: 'st-footer'},
-        { title: 'Gallery', filter: 'st-gallery'},
-        { title: 'Header', filter: 'st-header'},
-        { title: 'Hero', filter: 'st-hero'},
-        { title: 'Pricing', filter: 'st-pricing'},
-        { title: 'Statistic', filter: 'st-statistic'},
-        { title: 'Step', filter: 'st-step'},
-        { title: 'Team', filter: 'st-team'},
-        { title: 'Testimonial', filter: 'st-testimonial'},
+        { title: 'All', filter: 'js-snippet' },
+        { title: 'Blog', filter: 'st-blog' },
+        { title: 'Contact', filter: 'st-contact' },
+        { title: 'Content', filter: 'st-content' },
+        { title: 'CTA', filter: 'st-cta' },
+        { title: 'E-Commerce', filter: 'st-ecommerce' },
+        { title: 'Feature', filter: 'st-feature' },
+        { title: 'Footer', filter: 'st-footer' },
+        { title: 'Gallery', filter: 'st-gallery' },
+        { title: 'Header', filter: 'st-header' },
+        { title: 'Hero', filter: 'st-hero' },
+        { title: 'Pricing', filter: 'st-pricing' },
+        { title: 'Statistic', filter: 'st-statistic' },
+        { title: 'Step', filter: 'st-step' },
+        { title: 'Team', filter: 'st-team' },
+        { title: 'Testimonial', filter: 'st-testimonial' },
       ]
     },
   },
   watch: {
     itemsDest(items, oldItems) {
-      this.itemsDest.forEach(item => {
-        if(!item.id) {
-          item.id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)
-          const badElement = document.querySelector('.js-droppable > .js-snippet:not(.vue-element)')
+      this.itemsDest.forEach((item) => {
+        if (!item.id) {
+          item.id = Math.random()
+            .toString(36)
+            .replace(/[^a-z]+/g, '')
+            .substr(0, 5)
+          const badElement = document.querySelector(
+            '.js-droppable > .js-snippet:not(.vue-element)'
+          )
           badElement.remove()
         }
       })
-    }
+    },
   },
   created() {
     if (this.$dragula) {
@@ -184,10 +218,10 @@ export default {
       service.options('first', {
         revertOnSpill: true,
         copySortSource: false,
-        copy (el, source) {
+        copy(el, source) {
           return source.classList.contains('js-snippets')
         },
-        accepts (el, target, source, sibling) {
+        accepts(el, target, source, sibling) {
           return !target.classList.contains('js-snippets')
         },
       })
@@ -195,11 +229,11 @@ export default {
   },
   mounted() {
     if (typeof window !== 'undefined') {
-      ["resize", "load"].forEach((event) => {
+      ;['resize', 'load'].forEach((event) => {
         window.addEventListener(event, () => {
-          this.masonry(".js-snippets", ".js-snippet", 0, 2, 2, 1);
-        });
-      });
+          this.masonry('.js-snippets', '.js-snippet', 0, 2, 2, 1)
+        })
+      })
     }
   },
   methods: {
@@ -210,36 +244,35 @@ export default {
       let gHeight = 0
 
       for (let i = 0; i < gcLength; ++i) {
-        gHeight += gc[i].offsetHeight + parseInt(gridGutter);
+        gHeight += gc[i].offsetHeight + parseInt(gridGutter)
       }
 
       if (window.screen.width >= 1024) {
-        this.gHeight = gHeight / dGridCol + gHeight / (gcLength + 1) + 100 + 'px'
+        this.gHeight =
+          gHeight / dGridCol + gHeight / (gcLength + 1) + 100 + 'px'
       } else if (window.screen.width < 1024 && window.screen.width >= 768) {
         this.gHeight = gHeight / tGridCol + gHeight / (gcLength + 1) + 'px'
       } else {
         this.gHeight = gHeight / mGridCol + gHeight / (gcLength + 1) + 'px'
       }
-      console.log(this.gHeight)
-    }
-  }
+    },
+  },
 }
 </script>
 
 <style lang="scss">
 .drag-image {
-
 }
-.js-droppable>.js-snippet {
+.js-droppable > .js-snippet {
   width: 100%;
   padding: 0;
 }
 
-.js-droppable>.js-snippet:hover>.js-delete-btn {
+.js-droppable > .js-snippet:hover > .js-delete-btn {
   display: block;
 }
 
-.js-droppable>.js-snippet>img:hover {
+.js-droppable > .js-snippet > img:hover {
   transform: none;
   border-color: #dae1e7;
 }
